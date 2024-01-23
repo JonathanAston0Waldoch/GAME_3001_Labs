0ba15299f81b447bae6120496015919c77e4fefa
JonathanAston0Waldoch <jtaw@rogers.com>
2024-01-23 11:05:23 -0500

    update

---
+++ GAME3001_Lab2_Start/Assets/_MyAssets/_Scripts/AgentObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentObject : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+
+    public Vector3 TargetPosition
+    {
+        get { return m_target.position; }
+        set { m_target.position = value; }
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("starting Agent ...");
+        TargetPosition = m_target.position;
+    }
+}
